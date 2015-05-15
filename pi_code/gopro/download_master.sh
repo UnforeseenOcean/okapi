ed587dd2108e1d13bcd10c0c0e4f4c9fd1683eab
Jer Thorp <blprnt@blprnt.com>
2015-05-15 14:27:29 +0200

    Changing download master to point to correct files.

--- pi_code/gopro/download_master.sh
+++ pi_code/gopro/download_master.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-/home/pi/okavango/gopro/./download_w2.sh &
-/home/pi/okavango/gopro/./download_b1.sh &
-/home/pi/okavango/gopro/./download_w1.sh &
+/home/pi/okapi/pi_code/gopro/./download_w2.sh &
+/home/pi/okapi/pi_code/gopro/./download_b1.sh &
+/home/pi/okapi/pi_code/gopro/./download_w1.sh &
 
 wait
 echo "3 cameras complete"
