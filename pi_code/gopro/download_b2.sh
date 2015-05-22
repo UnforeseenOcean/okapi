ab97fd97fe852c37281812a103ff4b1d302bd231
Jer Thorp <blprnt@blprnt.com>
2015-05-22 22:00:39 +0200

    Server side changes.

--- pi_code/gopro/download_b2.sh
+++ pi_code/gopro/download_b2.sh
@@ -1,46 +1,52 @@
 #!/bin/bash
 
-#cam 3
+#1 black cam - 10.5.5.9
 
 todaysDir="$(date +'%d%m%y')"
 
-ping -I wlan2 -c 3 10.5.5.10>>/dev/null
+ping -c 3 -I wlan2 10.5.5.9>>/dev/null
 
 if [ $? -eq 0 ]; then
-	echo "Cam3 reachable"
+	echo "Cam2 reachable"
 	#turn it on
-	curl -s --interface wlan2 "http://10.5.5.10/bacpac/PW?t=hudsuckerb2&p=%01"
+	curl -s --interface wlan2 "http://10.5.5.9/bacpac/PW?t=hudsuckerb2&p=%01"
 	sleep 8
 	#make sure we're in photo mode
-	curl -s --interface wlan2 "http://10.5.5.10/camera/CM?t=hudsuckerb2&p=%01"
+	curl -s --interface wlan2 "http://10.5.5.9/camera/CM?t=hudsuckerb2&p=%01"
+	sleep 2
         #take a picture
-        sleep 2
-	curl -s --interface wlan2 "http://10.5.5.10/bacpac/SH?t=hudsuckerb2&p=%01"
-	sleep 8 
-	curl -s --interface wlan2 http://10.5.5.10:8080/videos/DCIM/100GOPRO/ > temp3.html
-	files=( $(python /home/pi/okapi/pi_code/gopro/html_parse.py temp3.html | grep JPG) )
+	curl -s --interface wlan2 "http://10.5.5.9/bacpac/SH?t=hudsuckerb2&p=%01"
+	sleep 8
+	curl -s --interface wlan2 http://10.5.5.9:8080/videos/DCIM/102GOPRO/ > temp1.html
+	files=( $(python /home/pi/okapi/pi_code/gopro/html_parse.py temp1.html | grep JPG) )
 	for i in "${files[@]}"
 	do
 		:
 		fileN="${i%.*}"
 		timestamp="$(date +'%H%M')"
-        if [ -e /home/pi/okapi/pi_code/okanode/public/archive/${todaysDir}/jpg/left ]; then
-		   if ( ls /home/pi/okapi/pi_code/okanode/public/archive/${todaysDir}/jpg/left/${fileN}* >/dev/null 2>&1 ) || ( ls /home/pi/okapi/pi_code/okanode/public/uploads/jpg/left/${fileN}* >/dev/null 2>&1 ); then
-			  echo "File exists, will not download"
-		   else
-              curl -s --interface wlan2 "http://10.5.5.10:8080/videos/DCIM/100GOPRO/${i}" > "/home/pi/okapi/pi_code/okanode/public/uploads/jpg/left/${fileN}_${todaysDir}${timestamp}.jpg"
-           fi
+		if [ -e /home/pi/okapi/pi_code/okanode/public/archive/${todaysDir}/jpg/gopro ]; then 
+			if  ls /home/pi/okapi/pi_code/okanode/public/archive/${todaysDir}/jpg/gopro/${fileN}left* 1>/dev/null 2>&1 ; then  
+				echo "File exists, will not download"
+			elif ls /home/pi/okapi/pi_code/okanode/public/uploads/jpg/gopro/${fileN}left* 1>/dev/null 2>&1 ; then
+				echo "File exists, will not download"
+			else
+				#curl -s --interface wlan2 "http://10.5.5.9:8080/videos/DCIM/102GOPRO/${i}" > "/home/pi/okavango/okanode/public/uploads/jpg/gopro/${fileN}left_${todaysDir}${timestamp}.jpg"
+				curl -s --interface wlan2 "http://10.5.5.9:8080/videos/DCIM/102GOPRO/${i}" > "/home/pi/okapi/pi_code/okanode/public/uploads/jpg/gopro/${fileN}left_${todaysDir}${timestamp}.jpg"
+			fi
 		else
-		if ls /home/pi/okapi/pi_code/okanode/public/uploads/jpg/left/${fileN}* 1> /dev/null 2>&1; then
-	              echo "File already queued to upload"
-        	else	
-		      curl -s --interface wlan2 "http://10.5.5.10:8080/videos/DCIM/100GOPRO/${i}" > "/home/pi/okapi/pi_code/okanode/public/uploads/jpg/left/${fileN}_${todaysDir}${timestamp}.jpg"
-           fi
+			if ls /home/pi/okapi/pi_code/okanode/public/uploads/jpg/gopro/${fileN}left* 1> /dev/null 2>&1; then
+				echo "File already queued to upload"
+			else	
+				#curl -s --interface wlan2 "http://10.5.5.9:8080/videos/DCIM/102GOPRO/${i}" > "/home/pi/okavango/okanode/public/uploads/jpg/gopro/${fileN}left_${todaysDir}${timestamp}.jpg"
+				curl -s --interface wlan2 "http://10.5.5.9:8080/videos/DCIM/102GOPRO/${i}" > "/home/pi/okapi/pi_code/okanode/public/uploads/jpg/gopro/${fileN}left_${todaysDir}${timestamp}.jpg"
+				echo "Downloading ${fileN}"
+			fi
 		fi
 	done
-	rm temp3.html
+	rm temp1.html
 	#turn it off 
-	curl -s --interface wlan2 "http://10.5.5.10/bacpac/PW?t=hudsuckerb2&p=%00"
+	curl -s --interface wlan2 "http://10.5.5.9/bacpac/PW?t=hudsuckerb2&p=%00"
 else 
-	echo "Could not reach cam3"
+	echo "Could not reach cam2"
 fi 
+
